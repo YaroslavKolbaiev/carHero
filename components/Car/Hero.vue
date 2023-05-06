29dc518dacc992c17629b4990a11795514861d15
Yaroslav Kolbaiev <103733788+YaroslavKolbaiev@users.noreply.github.com>
2023-05-06 12:53:36 +0300

    fix: buttons, main page

--- components/Car/Hero.vue
+++ components/Car/Hero.vue
@@ -2,17 +2,18 @@
   <div
     class="relative grow bg-no-repeat bg-cover bg-bottom bg-[url('https://images.unsplash.com/photo-1506610654-064fbba4780c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')]"
   >
-    <div class="z-10 w-full absolute h-full flex justify-center items-center">
+    <div class="z-10 w-full absolute h-full flex justify-center pt-20">
       <div class="text-center w-1/2">
         <h1 class="text-8xl font-serif mb-10 text-white">Find your car</h1>
         <CarSearchBar />
-        <div class="mt-5">
+        <!-- <div class="mt-5">
           <NuxtLink
             class="text-white text-3xl underline hover:text-sky-200 transition duration-200 ease"
             to="/cars"
             >See all cars
           </NuxtLink>
-        </div>
+        </div> -->
+        <div class="text-white mt-10">Will be carousel here</div>
       </div>
     </div>
     <div class="z-1 absolute w-full h-full bg-black opacity-50"></div>
